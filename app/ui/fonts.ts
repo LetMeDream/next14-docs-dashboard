527c7cd50f52e83d5f9536dfa0822457ec802375
Raul <raulalfonzo66@gmail.com>
2024-02-03 19:30:01 -0400

    chore(fonst): export fonts from 'fonts.ts' barrel file into the components that require them (layout.tsx and acme-logo.tsx)

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lucy = Lusitana({ style: 'normal', weight: '700', subsets: ['latin'] })
