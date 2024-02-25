c6ee84bda8f1a7895bb294f66b48c6c1734adb88
Raul <raulalfonzo66@gmail.com>
2024-02-25 12:38:38 -0400

    Import and use redirect and revalidatePath from next/navigation and /cache, respectively

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
   id: z.string(),
@@ -32,5 +34,6 @@ export const createInvoice = async (formData: FormData)  => {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 
-  
+    revalidatePath('/dashboard/invoices/')
+    redirect('/dashboard/invoices/')
 }
