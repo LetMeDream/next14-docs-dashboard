10705bc9a6d50bd592ab1dd72f2c8b3ba05a19e7
Raul <raulalfonzo66@gmail.com>
2024-02-28 00:31:20 -0400

    feat (change status): import 'changeStatus' server action. Bind the id to it and create the newStatus state variable that will be the value SET in the UPDATE call.

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,29 +1,68 @@
+'use client';
+
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import { changeStatus } from '@/app/lib/actions';
+import { useState, useEffect } from 'react';
 
-export default function InvoiceStatus({ status }: { status: string }) {
+export default function InvoiceStatus({
+   status,
+   invoice, 
+   index
+  }: { 
+    status: string, 
+    invoice: {
+      status: string,
+      id: string
+    },
+    index: number
+  }) {
+  
+  const onChangeStatus = () => {
+    const form: HTMLFormElement = document.getElementById(`form_${index}`) as HTMLFormElement
+    form.requestSubmit()
+  }
+  const bindChangeStatus = changeStatus.bind(null, invoice?.id.trim())
+  const [newStatus, setNewStatus] = useState('')
+
+  useEffect(() => {
+    setNewStatus(() => {
+      return invoice.status === 'paid' ? 'pending' : 'paid'
+    })
+  }, [invoice.status])
+
   return (
-    <span
-      className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
-        {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
-      )}
-    >
-      {status === 'pending' ? (
-        <>
-          Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
-        </>
-      ) : null}
-      {status === 'paid' ? (
-        <>
-          Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
-        </>
-      ) : null}
-    </span>
+    <>
+      <span
+        className={clsx(
+          'inline-flex items-center rounded-full px-2 py-1 text-xs cursor-pointer relative',
+          {
+            'bg-gray-100 text-gray-500': status === 'pending',
+            'bg-green-500 text-white': status === 'paid',
+          },
+        )}
+        onClick={onChangeStatus}
+      > 
+        {status === 'pending' ? (
+          <>
+            Pending
+            <ClockIcon className="ml-1 w-4 text-gray-500" />
+          </>
+        ) : null}
+        {status === 'paid' ? (
+          <>
+            Paid
+            <CheckIcon className="ml-1 w-4 text-white" />
+          </>
+        ) : null}
+      </span>
+      <form 
+        id={`form_${index}`}
+        className='hidden'
+        action={bindChangeStatus}
+      > 
+        <input id='status' name='status' defaultValue={newStatus} />
+      </form>
+    </>
   );
 }
